b275b5452cb9a69254687c4e0ee77773779e8546
Clément Hurlin <smelc@users.noreply.github.com>
2024-07-10 12:28:42 +0200

    [python] Crawler log folder

---
+++ src/crawler_log_folder.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        result = 0
+        for i in range(len(logs)):
+            match logs[i]:
+                case "../":
+                    result = max(0, result - 1)
+                case "./":
+                    pass
+                case _:
+                    result += 1
+        return result 
